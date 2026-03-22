5e1a58b2f618ec397efa1c06e8391b158c52d9f4
TadiwansheMashongwa <mashongwatadiwanashe@gmail.com>
2026-03-22 17:12:29 +0100

    feat(entity): add user entity with id field

--- src/test/java/com/tadiwanashe/authservice/entity/User.java
+++ src/test/java/com/tadiwanashe/authservice/entity/User.java
@@ -1,4 +1,22 @@
 package com.tadiwanashe.authservice.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
