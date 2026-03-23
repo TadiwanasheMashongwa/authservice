60e1c1255b730d3976b866289defb27719260d71
TadiwansheMashongwa <mashongwatadiwanashe@gmail.com>
2026-03-23 10:07:17 +0100

    feat(entity): add username field to User

--- src/test/java/com/tadiwanashe/authservice/entity/User.java
+++ src/test/java/com/tadiwanashe/authservice/entity/User.java
@@ -9,8 +9,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false )
+    private String username;
 
     public User() {}
+   public String getUsername(){
+        return this.username;
+   }
+   public void setUsername(String username){
+        this.username=username;
+   }
 
     public Long getId() {
         return id;
