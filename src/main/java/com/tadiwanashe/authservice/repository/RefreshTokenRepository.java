a610360f9d32e0a6a5facac00872450037528e1c
TadiwansheMashongwa <mashongwatadiwanashe@gmail.com>
2026-04-05 15:13:32 +0200

    feat(repository): add RefreshTokenRepository with findByToken and deleteByUser

---
+++ src/main/java/com/tadiwanashe/authservice/repository/RefreshTokenRepository.java
@@ -0,0 +1,16 @@
+package com.tadiwanashe.authservice.repository;
+
+import com.tadiwanashe.authservice.entity.RefreshToken;
+import com.tadiwanashe.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
+}
