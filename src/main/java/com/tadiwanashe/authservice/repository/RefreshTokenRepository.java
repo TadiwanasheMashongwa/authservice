6789cbef42a772bfd3a8729c7b8e2b5e2bec9c96
TadiwansheMashongwa <mashongwatadiwanashe@gmail.com>
2026-04-06 10:54:48 +0200

    fix(auth): force delete of existing refresh token using explicit JPQL query

--- src/main/java/com/tadiwanashe/authservice/repository/RefreshTokenRepository.java
+++ src/main/java/com/tadiwanashe/authservice/repository/RefreshTokenRepository.java
@@ -4,6 +4,8 @@ import com.tadiwanashe.authservice.entity.RefreshToken;
 import com.tadiwanashe.authservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,6 +14,7 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String token);
-    @Modifying
-    void deleteByUser(User user);
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM RefreshToken rt WHERE rt.user = :user")
+    void deleteByUser(@Param("user") User user);
 }
